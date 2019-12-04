a81e63696631285407bf3e06a0637e3c780590c5
theMistletoe <katachiwotodomete@gmail.com>
2019-12-04 22:24:27 +0900

    fix: remove dotenv and use create-react-app env

--- src/utils/libs/firebase.js
+++ src/utils/libs/firebase.js
@@ -1,15 +1,13 @@
-import * as dotenv from 'dotenv';
-dotenv.config()
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: '',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
